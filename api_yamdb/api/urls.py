991b7c25b8f129ecfa6bf21cb3c338bc71ae8ad5
SafonovaEkaterina <103047547+SafonovaEkaterina@users.noreply.github.com>
2023-02-19 17:15:19 +0300

    Revert "Revert "add coments""

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,20 +1,22 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from api.views import (
-    GetTokenView,
-    SignView,
-    UserViewSet,
-    CategoryViewSet,
-    GenreViewSet,
-    TitleViewSet
-)
+from .views import (GetTokenView, SignView, UserViewSet, 
+                    CommentViewSet, ReviewViewSet, GenreViewSet,
+                    TitleViewSet, CategoryViewSet)
 
 app_name = 'api'
 
 router_v1 = DefaultRouter()
 
 router_v1.register('users', UserViewSet, basename='users')
+router_v1.register(r'^titles/(?P<title_id>\d+)/reviews', ReviewViewSet,
+                   basename='reviews')
+router_v1.register(
+    r'^titles/(?P<title_id>\d+)/reviews/(?P<review_id>\d+)/comments',
+    CommentViewSet,
+    basename='comments'
+)
 router_v1.register('categories', CategoryViewSet, basename='categories')
 router_v1.register('genres', GenreViewSet, basename='genres')
 router_v1.register('titles', TitleViewSet, basename='titles')
